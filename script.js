c1c13229c61d281dba20968b5b12549f989e16f5
kherma <kamil.herma01@gmail.com>
2020-10-29 00:51:55 +0100

    Set initial game conditions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting players scores
+const scorePlayerOne = document.getElementById('score--0');
+const scorePlayerTwo = document.getElementById('score--1');
+scorePlayerOne.textContent = 0;
+scorePlayerTwo.textContent = 0;
+
+// Select dice
+const dice = document.querySelector('.dice');
+dice.classList.add('hide');
