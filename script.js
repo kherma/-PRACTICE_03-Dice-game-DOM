ef2a430613023fc305b8b0917ca7ecb753a98e51
kherma <kamil.herma01@gmail.com>
2020-10-29 01:36:31 +0100

    Implement current score update feature

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 'use strict';
 
+// Declare current score
+let currentScore = 0;
+
+// Declare active player
+// 0 - player one, 1 - player two
+// Initial value is 0 because player one always starts the game
+let activePlayer = 0;
+
+// Declare players scores
+let playerScores = [0, 0];
+
 // Selecting players scores
 const scorePlayerOne = document.getElementById('score--0');
 const scorePlayerTwo = document.getElementById('score--1');
-scorePlayerOne.textContent = 0;
-scorePlayerTwo.textContent = 0;
+scorePlayerOne.textContent = playerScores[0];
+scorePlayerTwo.textContent = playerScores[1];
+
+// const currentPlayerOne = document.getElementById('current--0');
+// const currentPlayerTwo = document.getElementById('current--1');
 
 // Select players
 const selectPlayerOne = document.querySelector('.player--0');
@@ -19,6 +33,7 @@ const btnNewGame = document.querySelector('.btn--new');
 const btnRollDice = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// Dice Roll function
 btnRollDice.addEventListener('click', function(){
 
     // 1. Generating random number in range 1 to 6
@@ -28,10 +43,16 @@ btnRollDice.addEventListener('click', function(){
     dice.classList.remove('hide');
     dice.src = `dice-${randomNumber}.png`
 
-    // 3. Check for rollde 1 True ? change player : hold/roll
+    // 3. Check for rollde 1 True ? change player : update current score
     if (randomNumber === 1) {
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        activePlayer = activePlayer === 0 ? 1 : 0;
         selectPlayerOne.classList.toggle('player--active');
         selectPlayerTwo.classList.toggle('player--active');
         dice.classList.add('hide');
+    } else {
+        currentScore += randomNumber;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     }
 });
